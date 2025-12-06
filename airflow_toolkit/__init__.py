fab8d754678ebb7f1cdeb2e7b44c744c89e07ec9
IADJALILProject <salahbeydjalil@gmail.com>
2025-12-06 17:01:45 +0100

    v2

--- airflow_toolkit/__init__.py
+++ airflow_toolkit/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from . import operators, hooks, utils
-
-__all__ = ["__version__", "operators", "hooks", "utils"]
+__all__ = ["__version__"]
 
 __version__ = "0.2.0"
